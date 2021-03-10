06e26b3289b842309b197967ce51a7714ea5b677
Moexin <moexin@qq.com>
2021-03-10 18:47:10 +0800

    Update

--- config.js
+++ config.js
@@ -1,1 +1,37 @@
-window.Config={SiteName:"TCat云服务状态监控",SiteUrl:"/",ApiKeys:["ur731670-58aac39ae057171ae725476e",],ShowLink:false,CountDays:30,Navi:[{text:"TCat云服务",url:"https://tcat.pw"}]};
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'TCat云服务状态监控',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api 域名
+  // 只需填写域名部分，默认为官网域名
+  // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
+  // 详见 https://github.com/yb/uptime-status/ 说明
+  ApiDomain: 'status.tcat.workers.dev',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'ur731670-58aac39ae057171ae725476e',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: false,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 30,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'TCat云服务',
+      url: 'https://tcat.pw'
+    }
+  ]
+};
